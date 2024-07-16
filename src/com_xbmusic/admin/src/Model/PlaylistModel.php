5e5b78c8ada36d715fae386856e2a41e3a1e9bc1
Roger CO <roger@crosborne.uk>
2024-07-16 22:52:19 +0100

    v0.0.11.6 wip on playlists
    edit view and form to do

---
+++ src/com_xbmusic/admin/src/Model/PlaylistModel.php
@@ -0,0 +1,329 @@
+<?php
+/*******
+ * @package xbMusic
+ * @filesource admin/src/Model/PlaylistModel.php
+ * @version 0.0.11.6 16th July 2024
+ * @author Roger C-O
+ * @copyright Copyright (c) Roger Creagh-Osborne, 2024
+ * @license GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html 
+ ******/
+
+namespace Crosborne\Component\Xbmusic\Administrator\Model;
+
+defined('_JEXEC') or die;
+
+use Joomla\CMS\Component\ComponentHelper;
+use Joomla\CMS\Factory;
+use Joomla\CMS\Filter\InputFilter;
+use Joomla\CMS\Form\Form;
+use Joomla\CMS\Helper\TagsHelper;
+use Joomla\CMS\Language\Text;
+use Joomla\CMS\MVC\Model\AdminModel;
+use Joomla\CMS\Plugin\PluginHelper;
+use Joomla\CMS\String\PunycodeHelper;
+use Joomla\CMS\Table\Table;
+use Joomla\CMS\Table\TableInterface;
+use Joomla\CMS\UCM\UCMType;
+use Joomla\Component\Fields\Administrator\Helper\FieldsHelper;
+use Joomla\Database\ParameterType;
+use Joomla\Filter\OutputFilter;
+use Joomla\Registry\Registry;
+use Joomla\Component\Categories\Administrator\Helper\CategoriesHelper;
+use Crosborne\Component\Xbmusic\Administrator\Helper\XbmusicHelper;
+use \SimpleXMLElement;
+use Symfony\Component\Validator\Constraints\IsNull;
+
+class PlaylistModel extends AdminModel {
+  
+    public $typeAlias = 'com_xbmusic.playlist';
+    
+    protected $xbmusic_batch_commands = array(
+        'untag' => 'batchUntag',
+    );
+    
+    public function batch($commands, $pks, $contexts) {
+        $this->batch_commands = array_merge($this->batch_commands, $this->xbmusic_batch_commands);
+        return parent::batch($commands, $pks, $contexts);
+    } 
+    
+    protected function batchUntag($value, $pks, $contexts) {
+        $taghelper = new TagsHelper();
+        $message = 'tag:'.$value.' removed from playlists :';
+        foreach ($pks as $pk) {
+            if ($this->user->authorise('core.edit', $contexts[$pk])) {
+                $existing = $taghelper->getItemTags('com_xbmusic.playlist', $pk, false);
+                $oldtags = array_column($existing,'tag_id');
+                $newtags = array();
+                for ($i = 0; $i<count($oldtags); $i++) {
+                    if ($oldtags[$i] != $value) {
+                        $newtags[] = $oldtags[$i];
+                    }
+                }
+                $params = array( 'id' => $pk, 'tags' => $newtags );
+                
+                if($this->save($params)){
+                    $message .= ' '.$pk;
+                }
+            } else {
+                $this->setError(Text::_('JLIB_APPLICATION_ERROR_BATCH_CANNOT_EDIT'));
+                return false;
+            }
+            Factory::getApplication()->enqueueMessage($message);
+        }
+        return true;
+    }
+    
+    protected function canDelete($record) {
+        if (empty($record->id) || ($record->status != -2)) {
+            return false;
+        }
+        
+        return $this->getCurrentUser()->authorise('core.delete', 'com_xbmusic.playlist.' . (int) $record->id);
+    }
+    
+    protected function canEditState($record) {
+        $user = $this->getCurrentUser();
+        
+        // Check for existing playlist.
+        if (!empty($record->id)) {
+            return $user->authorise('core.edit.state', 'com_xbmusic.playlist.' . (int) $record->id);
+        }
+        
+        // New playlist, so check against the category.
+        if (!empty($record->catid)) {
+            return $user->authorise('core.edit.state', 'com_xbmusic.category.' . (int) $record->catid);
+        }
+        
+        // Default to component settings if neither playlist nor category known.
+        return parent::canEditState($record);
+    }
+    
+    protected function prepareTable($table) {
+        
+        // Reorder the playlists within the category so the new playlist is first
+        if (empty($table->id)) {
+            $table->reorder('catid = ' . (int) $table->catid . ' AND status >= 0');
+        }
+    }
+    
+    public function getItem($pk = null) {
+        if ($item = parent::getItem($pk)) {
+            if (!empty($item->id)) {
+                $tagsHelper = new TagsHelper();
+                $item->tags = $tagsHelper->getTagIds($item->id, 'com_xbmusic.playlist');  
+                if ($item->type == 2) {
+                    $item->groupmembers = XbmusicHelper::getGroupMembers($item->id);
+                }
+            }
+        }        
+        return $item;
+    }
+    
+    public function getForm($data = [], $loadData = true) {
+        $app  = Factory::getApplication();
+        $params = ComponentHelper::getParams('com_xbmusic');
+        
+        // Get the form.
+        $form = $this->loadForm('com_xbmusic.playlist', 'playlist', ['control' => 'jform', 'load_data' => $loadData]);
+        
+        if (empty($form)) {
+            return false;
+        }
+        
+        //dynamically add fields for any taggroups defined in options and add the tags for them
+        $tagsarr = explode(',',$form->getValue('tags',null,''));
+        $parentids = $params->get('playlisttagparents',[]);
+        if (!empty($parentids)) {
+            $taghelp = new TagsHelper;
+            $parr = $taghelp->getTags($parentids);
+            foreach ($parr as $pid=>$parent) {
+                $groupname = $parent.'_tags';
+                $element = new SimpleXMLElement('<field name="'.$groupname.'" type="xbtags" label="'.ucfirst($parent).' Group" mode="nested" multiple="true" custom="deny" parent="'.$pid.'" />');
+                $form->setField($element, null, true, 'taggroups');
+                if (!empty($tagsarr)){
+                    $groupnametags = $taghelp->getTagTreeArray($pid);
+                    $grouptags = array_intersect($groupnametags, $tagsarr);
+                    $form->setValue($groupname,null,$grouptags);
+                }
+            }
+        } // endforeach parenttag
+        
+        return $form;
+    }
+    
+    protected function loadFormData() {
+        // Check the session for previously entered form data.
+        $app  = Factory::getApplication();
+        $data = $app->getUserState('com_xbmusic.edit.playlist.data', []);
+        
+        if (empty($data)) {
+            $data = $this->getItem();
+            $data->tracklist = $this->getPlaylistTrackList();
+            
+            $retview = $app->input->get('retview','');
+            // Pre-select some filters (Status, Category, Language, Access) in edit form if those have been selected in Article Manager: Articles
+            if (($this->getState('playlist.id') == 0) && ($retview != '')) {
+                $filters = (array) $app->getUserState('com_xbmusic.'.$retview.'.filter');
+                $data->set(
+                    'status',
+                    $app->getInput()->getInt(
+                            'status',
+                        ((isset($filters['status']) && $filters['status'] !== '') ? $filters['status'] : null)
+                        )
+                    );
+                    $data->set('catid', $app->getInput()->getInt('catid', (!empty($filters['category_id']) ? $filters['category_id'] : null)));
+
+                     if ($app->isClient('administrator')) {
+                         $data->set('language', $app->getInput()->getString('language', (!empty($filters['language']) ? $filters['language'] : null)));
+                     }
+    
+                    $data->set(
+                        'access',
+                        $app->getInput()->getInt('access', (!empty($filters['access']) ? $filters['access'] : $app->get('access')))
+                        );
+                }
+            }
+            
+            // If there are params fieldsets in the form it will fail with a registry object
+            if (isset($data->params) && $data->params instanceof Registry) {
+                $data->params = $data->params->toArray();
+            }
+            
+            return $data;
+        }
+        
+    public function save($data) {
+        $app    = Factory::getApplication();
+        $input  = $app->getInput();
+        $params = ComponentHelper::getParams('com_xbmusic');
+        $filter = InputFilter::getInstance();
+        $infomsg = '';
+        $warnmsg = '';
+
+        if ($input->get('task') == 'save2copy') {
+            $origTable = clone $this->getTable();
+            $origTable->load($input->getInt('id'));
+            
+            if ($data['name'] == $origTable->name) {
+                list($name, $alias) = $this->generateNewTitle($data['catid'], $data['alias'], $data['name']);
+                $data['name'] = $name;
+                $data['alias'] = $alias;
+            } else {
+                if ($data['alias'] == $origTable->alias) {
+                    $data['alias'] = '';
+                }
+            }
+            //need to add tracklinks
+            $this->storePlaylistTracks($origTable->id, $data['tracklist']);
+            // standard Joomla practice is to set the new copy record as unpublished
+            $data['status'] = 0;
+        }
+        
+       
+        //alias is the name so we'll set and check it every time
+        $newalias = OutputFilter::stringURLSafe($data['name']);
+        if (($data['id'] == 0) && XbmusicHelper::checkValueExists($newalias, '#__xbmusic_playlists', 'alias')) {
+            $warnmsg .= 'Duplicate alias - this playlist name is already in the database';
+            $app->enqueueMessage($warnmsg,'Error');
+            return false;
+        }
+        $data['alias'] = $newalias;        
+        
+        if (isset($data['created_by_alias'])) {
+            $data['created_by_alias'] = $filter->clean($data['created_by_alias'], 'TRIM');
+        }
+        
+        //merge any tag groups back into tags
+        $parentids = $params->get('playlisttagparents',[]);
+        if (!empty($parentids)) {
+            $thelp = new TagsHelper;
+            $parr = $thelp->getTags($parentids);
+            foreach ($parr as $pid=>$parent) {
+                $groupname = $parent.'_tags';
+                if (!empty($data[$groupname])) {
+                    $data['tags'] = ($data['tags']) ? array_unique(array_merge($data['tags'],$data[$groupname])) : $data[$groupname];
+                }
+            }
+        } // endforeach parenttag
+        
+        // ok ready to save the playlist data
+        if (parent::save($data)) {
+            $sid = $this->getState('playlist.id');
+            $this->storePlaylistTracks($sid, $data['tracklist']);
+            // Check possible workflow
+            if ($infomsg != '') $app->enqueueMessage($infomsg, 'Information');            
+            return true;
+        }
+        if ($infomsg != '') $app->enqueueMessage($infomsg, 'Information');
+        if ($warnmsg != '') $app->enqueueMessage($warnmsg, 'Warning');        
+        return false;
+    }
+    
+    protected function preprocessForm(Form $form, $data, $group = 'content') {
+        Factory::getApplication()->getSession()->set('playlistname', $data->name);
+        if ($this->canCreateCategory()) {
+            $form->setFieldAttribute('catid', 'allowAdd', 'true');
+            
+            // Add a prefix for categories created on the fly.
+            $form->setFieldAttribute('catid', 'customPrefix', '#new#');
+        }
+        
+        parent::preprocessForm($form, $data, $group);
+    }
+    
+    public function saveorder($idArray = null, $lft_array = null)
+    {
+        // Get an instance of the table object.
+        $table = $this->getTable();
+        
+        if (!$table->saveorder($idArray, $lft_array))
+        {
+            $this->setError($table->getError());
+            
+            return false;
+        }
+        
+        return true;
+    }
+    
+    private function canCreateCategory() {
+        return $this->getCurrentUser()->authorise('core.create', 'com_content');
+    }
+    
+    public function getPlaylistTrackList() {
+        $db = $this->getDbo();
+        $query = $db->getQuery(true);
+        $query->select('a.id as track_id, ba.note AS note, ba.listorder AS oldorder');
+        $query->from('#__xbmusic_playlisttrack AS ba');
+        $query->innerjoin('#__xbmusic_tracks AS a ON ba.track_id = a.id');
+        $query->where('ba.playlist_id = '.(int) $this->getItem()->id);
+        $query->order('a.title ASC');
+        $db->setQuery($query);
+        return $db->loadAssocList();
+    }
+    
+    function storePlaylistTracks($playlist_id, $trackList) {
+        //delete existing role list
+        $db = $this->getDbo();
+        $query = $db->getQuery(true);
+        $query->delete($db->quoteName('#__xbmusic_playlisttrack'));
+        $query->where('playlist_id = '.$playlist_id);
+        $db->setQuery($query);
+        $db->execute();
+        //restore the new list
+        foreach ($trackList as $trk) {
+            if ($trk['track_id'] > 0) {
+                $query = $db->getQuery(true);
+                $query->insert($db->quoteName('#__xbmusic_playlisttrack'));
+                $query->columns('playlist_id,track_id,note,listorder');
+                $query->values('"'.$playlist_id.'","'.$trk['track_id'].'","'.$trk['note'].'","'.$trk['oldorder'].'"');
+                //try
+                $db->setQuery($query);
+                $db->execute();
+            }
+        }
+    }
+
+    
+}
+
---
+++ src/com_xbmusic/admin/tmpl/playlist/edit.php
@@ -0,0 +1,193 @@
+<?php
+/*******
+ * @package xbMusic
+ * @filesource admin/tmpl/playlist/edit.php
+ * @version 0.0.11.6 16th July 2024
+ * @author Roger C-O
+ * @copyright Copyright (c) Roger Creagh-Osborne, 2024
+ * @license GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html 
+ ******/
+
+defined('_JEXEC') or die;
+
+use Joomla\CMS\Factory;
+use Joomla\CMS\Helper\TagsHelper;
+use Joomla\CMS\HTML\HTMLHelper;
+use Joomla\CMS\Language\Text;
+use Joomla\CMS\Layout\LayoutHelper;
+use Joomla\Registry\Registry;
+use Joomla\CMS\Router\Route;
+use Joomla\CMS\Uri\Uri;
+use Crosborne\Component\Xbmusic\Administrator\Helper\XbmusicHelper;
+
+/** @var Joomla\CMS\WebAsset\WebAssetManager $wa */
+$wa = $this->document->getWebAssetManager();
+$wa->useScript('keepalive')
+->useScript('form.validate')
+->useScript('xbmusic.showdown');
+
+// Create shortcut to parameters.
+//$params = clone $this->state->get('params');
+//$params->merge(new Registry($this->item->attribs));
+
+$input = Factory::getApplication()->getInput();
+
+?>
+<script>
+	function clearmd() {
+    	var descMd = document.getElementById('jform_description').value;
+    	var converter = new showdown.Converter();
+        var descHtml = converter.makeHtml(descMd);
+		var descText = stripHtml(descHtml);
+        document.getElementById('jform_description').value = descText;
+        updatePvMd();
+	}
+	
+    function stripHtml(html) {
+	   let doc = new DOMParser().parseFromString(html, 'text/html');
+	   return doc.body.textContent || "";
+    }
+
+	function updatePvMd() {
+    	var descText = document.getElementById('jform_description').value;
+		var converter = new showdown.Converter();
+        var descHtml = converter.makeHtml(descText);
+		document.getElementById('pv_desc').innerHTML= descHtml;
+    }
+
+ 	function postFolder() {
+ 		document.getElementById('task').value='track.setfolder';
+ 		this.form.submit();
+ 	}
+//     	var userdata = {'id':mydata,'name':myname};
+//         jQuery.ajax({
+//                 type: "POST",
+//                 url: "YOUR PHP URL HERE",
+//                 data:userdata, 
+//                 success: function(data){
+//                     console.log(data);
+//                 }
+//                 });
+</script>
+<div id="xbcomponent">
+    <form action="<?php echo Route::_('index.php?option=com_xbmusic&view=playlist&layout=edit&id='. (int) $this->item->id); ?>"
+    	method="post" name="adminForm" id="item-form" class="form-validate" >
+    	<div class="row form-vertical">
+    		<div class="col-md-10">
+            	<?php echo LayoutHelper::render('joomla.edit.title_alias', $this); ?>
+    		</div>
+    		<div class="col-md-2">
+    			<?php echo $this->form->renderField('id'); ?> 
+    		</div>
+    	</div>
+    	<hr />
+     <div class="main-card">
+        <?php echo HTMLHelper::_('uitab.startTabSet', 'myTab', ['active' => 'general', 'recall' => true]); ?>
+
+        <?php echo HTMLHelper::_('uitab.addTab', 'myTab', 'general', Text::_('General')); ?>
+			<div class="row form-vertical">
+           		<div class="col-12 col-lg-9">
+   					<div class="row xb09">
+		           		<div class="col-12 col-lg-5">
+						</div>   					
+		           		<div class="col-12 col-lg-7">
+        					<?php echo $this->form->renderField('comp_date'); ?> 
+        				</div>
+        			</div>
+  					<div class="row">
+		  	     		<div class="col-12 col-lg-6">
+        					<?php echo $this->form->renderField('description'); ?> 
+        				</div>
+		           		<div class="col-12 col-lg-6">
+		           			<div class="control-group"><div class="control-label" style="width:90%;">
+		           					<?php echo Text::_('Preview with Markdown formatting'); ?>
+		           				</div>
+								<div id="pv_desc" class="xbbox xbboxwht" style="height:23.5rem; overflow-y:scroll;">
+		           				</div>
+        					</div> 
+        				</div>
+        			</div>
+	   			</div>
+           		<div class="col-12 col-lg-3">
+        			<?php echo $this->form->renderField('status'); ?> 
+        			<?php echo $this->form->renderField('catid'); ?> 
+         			<?php echo $this->form->renderField('tags'); ?> 
+         			<?php echo $this->form->renderField('access'); ?> 
+        			<?php echo $this->form->renderField('ordering'); ?> 
+        			<?php echo $this->form->renderField('note'); ?> 
+           		</div>
+    		</div>
+         <?php echo HTMLHelper::_('uitab.endTab'); ?>
+
+        <?php echo HTMLHelper::_('uitab.addTab', 'myTab', 'taggroups', Text::_('Tag Groups')); ?>
+			<div class="row">
+				<?php echo $this->form->renderFieldset('taggroups'); ?>
+    		</div>
+         <?php echo HTMLHelper::_('uitab.endTab'); ?>
+
+        <?php echo HTMLHelper::_('uitab.addTab', 'myTab', 'links', Text::_('Linked Items')); ?>
+			<div class="row">
+				<div class="col-12">
+					<?php echo $this->form->renderField('tracklist'); ?>	
+				</div>
+				<div class="col-12">
+					Album
+				</div>
+				<div class="col-12">
+					Performers
+				</div>
+				<div class="col-12">
+					Playlists
+				</div>
+    		</div>
+            		<?php echo $this->form->renderField('ext_links');?>
+           		
+         <?php echo HTMLHelper::_('uitab.endTab'); ?>
+         
+
+
+        <?php echo HTMLHelper::_('uitab.addTab', 'myTab', 'publishing', Text::_('Publishing')); ?>
+        <div class="row">
+            <div class="col-12 col-lg-6">
+                <fieldset id="fieldset-publishingdata" class="options-form">
+                    <legend><?php echo Text::_('JGLOBAL_FIELDSET_PUBLISHING'); ?></legend>
+                    <div>
+                    <?php echo LayoutHelper::render('joomla.edit.publishingdata', $this); ?>
+                    </div>
+                </fieldset>
+            </div>
+            <div class="col-12 col-lg-6">
+                <fieldset id="fieldset-metadata" class="options-form">
+                    <legend><?php echo Text::_('JGLOBAL_FIELDSET_METADATA_OPTIONS'); ?></legend>
+                    <div>
+                    <?php echo LayoutHelper::render('joomla.edit.metadata', $this); ?>
+                    </div>
+                </fieldset>
+            </div>
+        </div>
+        <?php echo HTMLHelper::_('uitab.endTab'); ?>
+
+        <?php if ($this->canDo->get('core.admin') ) : ?>
+            <?php echo HTMLHelper::_('uitab.addTab', 'myTab', 'permissions', Text::_('Permissions')); ?>
+            <fieldset id="fieldset-rules" class="options-form">
+                <legend><?php echo Text::_('User Group Permissions'); ?></legend>
+                <div>
+                	<?php echo $this->form->getInput('rules'); ?>
+                </div>
+            </fieldset>
+            <?php echo HTMLHelper::_('uitab.endTab'); ?>
+        <?php endif; ?>
+
+        <?php echo HTMLHelper::_('uitab.endTabSet'); ?>
+    	<hr />
+    </div>	
+    <input type="hidden" name="task" id="task" value="playlist.edit" />
+    <?php echo HTMLHelper::_('form.token'); ?>
+    </form>
+    <div class="clearfix"></div>
+    <?php echo XbmusicHelper::credit('xbMusic');?>
+</div>
+<script>
+       updatePvMd();
+       document.getElementById("jform_description").addEventListener("input", (event) => updatePvMd());
+</script>
