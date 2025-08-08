445b38c1c7c3e408e8456e382e77915f86be878f
rogercreagh <44980188+rogercreagh@users.noreply.github.com>
2025-08-08 19:17:58 +0100

    0.0.58.0 install bug fixed

--- src/com_xbmusic/admin/sql/updates/mysql/0.0.58.0.sql
+++ src/com_xbmusic/admin/sql/updates/mysql/0.0.58.0.sql
@@ -1,1 +1,1 @@
-ALTER TABLE `#__xbmusic_playlists` ADD `allowdupes` INT(1) unsigned NOT NULL DEFAULT AFTER `publicschd`;
+ALTER TABLE `#__xbmusic_playlists` ADD `allowdupes` INT(1) unsigned NOT NULL DEFAULT '0' AFTER `publicschd`;
